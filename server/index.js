1e1a7210bb2384bd333c7d956160e4948476e6e9
Ramit Pandey <ramitpandey2002@gmail.com>
2026-01-01 00:17:41 +0530

    post path name change

--- server/index.js
+++ server/index.js
@@ -15,52 +15,50 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ text: "server is working" });
+  res.json({ text: "server is working" });
 });
 
-app.post("/shorten", async (req, res) => {
-    const { url } = req.body;
-    if (!url) return res.status(400).json({ error: "URL is required" });
+app.post("/create-url", async (req, res) => {
+  const { url } = req.body;
+  if (!url) return res.status(400).json({ error: "URL is required" });
 
-    const existing = await db
-        .select()
-        .from(urls)
-        .where(eq(urls.originalUrl, url))
-        .limit(1);
+  const existing = await db
+    .select()
+    .from(urls)
+    .where(eq(urls.originalUrl, url))
+    .limit(1);
 
-    if (existing.length > 0) {
-        return res
-            .status(200)
-            .json({ shortUrl: `${BaseUrl}/${existing[0].code}` });
-    }
-    const code = await createUniqueCode();
+  if (existing.length > 0) {
+    return res.status(200).json({ shortUrl: `${BaseUrl}/${existing[0].code}` });
+  }
+  const code = await createUniqueCode();
 
-    await db.insert(urls).values({
-        code,
-        originalUrl: url,
-    });
+  await db.insert(urls).values({
+    code,
+    originalUrl: url,
+  });
 
-    console.log(code);
+  console.log(code);
 
-    return res.json({ shortUrl: `${BaseUrl}/${code}` });
+  return res.json({ shortUrl: `${BaseUrl}/${code}` });
 });
 
 app.get("/:code", async (req, res) => {
-    const { code } = req.params;
+  const { code } = req.params;
 
-    const result = await db
-        .select()
-        .from(urls)
-        .where(eq(urls.code, code))
-        .limit(1);
+  const result = await db
+    .select()
+    .from(urls)
+    .where(eq(urls.code, code))
+    .limit(1);
 
-    if (result.length === 0) {
-        return res.status(404).send("URL not found");
-    }
+  if (result.length === 0) {
+    return res.status(404).send("URL not found");
+  }
 
-    res.redirect(result[0].originalUrl);
+  res.redirect(result[0].originalUrl);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
