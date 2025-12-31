560175956f5097bf0bd19405c2c0a979d1087ac1
Ramit Pandey <ramitpandey2002@gmail.com>
2026-01-01 00:19:25 +0530

    Update app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,57 +2,57 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [url, setUrl] = useState("");
-    const [shortUrl, setShortUrl] = useState("");
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const res = await fetch(
-            "https://url-shortener-beta-bice.vercel.app/shorten",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ url }),
-            }
-        );
-
-        const data = await res.json();
-        // console.log(data);
-        setShortUrl(data.shortUrl);
-    }
-    return (
-        <div className="main">
-            <h2>URL Shortener</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <label htmlFor="url" className="url-label">
-                        Enter Url:
-                    </label>
-                    <input
-                        type="url"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)}
-                        className="url-input"
-                        required
-                    />
-                </div>
-                <button type="submit" className="submit-btn">
-                    Submit
-                </button>
-            </form>
-
-            {shortUrl && (
-                <p>
-                    Short URL:{" "}
-                    <a href={shortUrl} target="_blank">
-                        {shortUrl}
-                    </a>
-                </p>
-            )}
-        </div>
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch(
+      "https://url-shortener-beta-bice.vercel.app/create-url",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      },
     );
+
+    const data = await res.json();
+    // console.log(data);
+    setShortUrl(data.shortUrl);
+  }
+  return (
+    <div className="main">
+      <h2>URL Shortener</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="url" className="url-label">
+            Enter Url:
+          </label>
+          <input
+            type="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            className="url-input"
+            required
+          />
+        </div>
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
+      </form>
+
+      {shortUrl && (
+        <p>
+          Short URL:{" "}
+          <a href={shortUrl} target="_blank">
+            {shortUrl}
+          </a>
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default App;
